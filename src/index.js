334da1d622aa0b93a43231783cee999c63ca9a6a
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-06-14 16:48:42 -0700

    No longer using canvas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const Sound = require("./sound.js");
 const GameView = require("./game_view.js");
 
 document.addEventListener("DOMContentLoaded", function(){
-  const canvasEl = document.getElementById("game-canvas");
   const menuModal = document.getElementsByClassName("menu");
   const soundButton = document.getElementById("music");
   const startButton = document.getElementById("start");
@@ -16,30 +15,14 @@ document.addEventListener("DOMContentLoaded", function(){
   const teamPage = document.getElementById("team-management");
   const music = new Sound;
   const game = new Game;
-  canvasEl.width = Game.DIM_X;
-  canvasEl.height= Game.DIM_Y;
   music.menuMusic.volume = 0.2;
 
-  let canvasPosition = canvasEl.getBoundingClientRect();
-  canvasEl.addEventListener('mousemove', function(e){
-    Game.MOUSE.x = e.x - canvasPosition.left;
-    Game.MOUSE.y = e.y - canvasPosition.top;
-  });
-
-  canvasEl.addEventListener('mouseleave', function(){
-    Game.MOUSE.x = undefined;
-    Game.MOUSE.y = undefined;
-  });
-
-  const ctx = canvasEl.getContext("2d");
   const gameView = new GameView(game, ctx);
 
   startButton.addEventListener('click', () => {
     menuModal[0].classList.add("hidden");
     game.addEnemy();
     game.setTurn();
-    gameView.animate();
-    gameView.createGrid();
   });
 
   creditButton.addEventListener('click', () => {
