76f6e89b64dd2037391a5ef479c33293b9a761c8
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-06-22 22:33:48 -0700

    Dry

--- src/character/enemies/slime.js
+++ src/character/enemies/slime.js
@@ -2,17 +2,17 @@ const Character = require("../char.js");
 
 class Slime extends Character{
   constructor(id, side = "enemy", charType = "Slime", level = 1, currentHealth = 30, maxHealth = 30, AP = 4, APRec = 4, power = 5, armor = 0,
-    initiative = 10, critChance = 0, critDamage = 2, damageMod = 0, healMod = 0, cdMod = 1, lifesteal = 0, regen = 5,
+    initiative = 10, critChance = 0, critDamage = 2, damageMod = 0, healMod = 0, cdMod = 0, lifesteal = 0, regen = 5,
     barrier = 0, barrierDecay = 0, physicalRes = 0, fireRes = 0, waterRes = 0, lightningRes = 0, poisonRes = 0, energyRes = 0, 
     holyRes = 0, darkRes = 0, physicalMod = 0, fireMod = 0, waterMod = 0, lightningMod = 0, poisonMod = 0,
     energyMod = 0, holyMod = 0, darkMod = 0, talents = [], conditions = [], xp = 0,
     maxXP = 100, xpReward = 0){
-    super(id, side, charType, level = 1, currentHealth = 30, maxHealth = 30, AP = 4, APRec = 4, power = 5, armor = 0,
-      initiative = 10, critChance = 0, critDamage = 2, damageMod = 0, healMod = 0, cdMod = 1, lifesteal = 0, regen = 5,
-      barrier = 0, barrierDecay = 0, physicalRes = 0, fireRes = 0, waterRes = 0, lightningRes = 0, poisonRes = 0, energyRes = 0, 
-      holyRes = 0, darkRes = 0, physicalMod = 0, fireMod = 0, waterMod = 0, lightningMod = 0, poisonMod = 0,
-      energyMod = 0, holyMod = 0, darkMod = 0, talents = [], conditions = [], xp = 0,
-      maxXP = 100, xpReward = 10);
+    super(id, side, charType, level,  currentHealth,  maxHealth, AP,  APRec,  power,  armor, 
+      initiative, critChance,  critDamage,  damageMod,  healMod,  cdMod,  lifesteal,  regen, 
+      barrier,  barrierDecay,  physicalRes,  fireRes,  waterRes,  lightningRes,  poisonRes,  energyRes,  
+      holyRes, darkRes,  physicalMod,  fireMod,  waterMod,  lightningMod,  poisonMod, 
+      energyMod, holyMod,  darkMod,  talents, conditions, xp, 
+      maxXP, xpReward);
   };
 }
 
